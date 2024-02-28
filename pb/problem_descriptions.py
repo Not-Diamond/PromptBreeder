5949fd78e4e6be46f0a538e9a3bc5aec9657691b
Tze-Yang Tung <r0ymanesco@gmail.com>
2024-02-28 14:12:31 +0000

    add problem desc for each dataset

---
+++ pb/problem_descriptions.py
@@ -0,0 +1,19 @@
+WITH_SUBSETS = ["bbh", "mmlu", "superglue"]
+
+problem_descriptions = {
+    "hellaswag": "Finish the sentence from multiple choice endings.",
+    "bbh": "This is a task believed to be beyond the capabilities of current language models.",
+    "ARC_c": "This is a grade-school level, multiple-choice science question.",
+    "ARC_e": "This is a grade-school level, multiple-choice science question.",
+    "gsm8k": "This is a grade school math word problem.",
+    "humaneval": "This is a coding problem.",
+    "mbpp": "This is a Python programming problem, designed to be solvable by entry level programmers, covering programming fundamentals, standard library functionality, and so on.",
+    "mmlu": "This is a question related to elementary mathematics, US history, computer science, law, and more.",
+    "piqa": "This is a question related to task of physical commonsense reasoning.",
+    "siqa": "This is a question related to social commonsense intelligence.",
+    "squadv2": "Answer the question if possible, but also determine when no answer is supported by the paragraph and abstain from answering.",
+    "superglue": "This is a difficult language understanding task.",
+    "winogrande": "The following is a difficult multiple-choice test.",
+    "xsum": "This is an extreme summarization task.",
+    "race": "This is a reading comprehension problem designed for middle school and high school students."
+}
