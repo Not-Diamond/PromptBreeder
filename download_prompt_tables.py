68fdf3d532f67b15843c6f7340e079b8037d19ec
Tze-Yang Tung <r0ymanesco@gmail.com>
2024-03-06 14:35:17 +0000

    add download prompt table script

---
+++ download_prompt_tables.py
@@ -0,0 +1,10 @@
+import wandb
+from dotenv import load_dotenv
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    with wandb.init(project="PromptBreeder") as run:
+        artifact = run.use_artifact("prompt-table:latest")
+        _ = artifact.download(root="datasets")
